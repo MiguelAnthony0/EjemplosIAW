117beb89d13e11297427f300ba1bff918a19510a
Miguel Anthony <miguelanthony.portillo.costa@gmail.com>
2023-11-24 17:22:45 +0100

    Numero mayor o menor .JS

---
+++ Pruebas.js
@@ -0,0 +1,11 @@
+
+var numero1 = prompt ("Dame un numero")
+var numero2 = prompt ("Dame un numero")
+
+if (numero1 > numero2 ) {
+    alert("El numero " + numero1 + " es mayor al numero " + numero2 )  
+  } else {
+    alert("El numero " + numero1 + " es menor al numero " + numero2 ) 
+  }
+
+
